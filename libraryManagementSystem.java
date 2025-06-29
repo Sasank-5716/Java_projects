1d6f5d0742627cce695bed295905e3fb32ac8bb9
Sasank5716 <sasanklama5716@gmail.com>
2025-06-29 15:32:19 +0545

    Add class book and attributes of the class

--- libraryManagementSystem.java
+++ libraryManagementSystem.java
@@ -5,6 +5,21 @@ import java.util.*;
 import java.text.SimpleDateFormat;
 import java.time.temporal.ChronoUnit;
 
+// Book class
+class Book {
+    private String id;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = true;
+    }
+}
+
 public class libraryManagementSystem {
     
 }
