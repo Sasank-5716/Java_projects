b8a1e881f0155e5448e5bf2055c5f38d4c6a87bd
Sasank5716 <sasanklama5716@gmail.com>
2025-07-06 11:30:32 +0545

    Improve login/signup UI

--- libraryManagementSystem.java
+++ libraryManagementSystem.java
@@ -98,6 +98,9 @@ public class libraryManagementSystem {
     }
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {}
         SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
     }
 
