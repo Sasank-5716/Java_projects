c8de39d497c431c18a430101e9177dbd5496afb5
Sasank5716 <sasanklama5716@gmail.com>
2025-07-09 19:36:59 +0545

    create class NumberGuessingGame

--- NumberGuessingGame/NumberGuessingGame.java
+++ NumberGuessingGame/NumberGuessingGame.java
@@ -2,3 +2,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
+
+public class NumberGuessingGame extends JFrame {
+    private int numberToGuess;
+    private int attempts;
+    private JTextField guessField;
+    private JLabel messageLabel;
+    private JButton guessButton;
+
+}
