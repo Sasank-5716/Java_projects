b62cfc590492c33544a1a115f158c9b6f06f7870
Sasank5716 <sasanklama5716@gmail.com>
2025-07-13 12:20:25 +0545

    Add TodoListApp class and method

--- TodoListApp/TodoListApp.java
+++ TodoListApp/TodoListApp.java
@@ -3,7 +3,18 @@ import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
 
+public class TodoListApp extends JFrame {
+    private DefaultListModel<Task> pendingModel;
+    private DefaultListModel<Task> completedModel;
+    private JList<Task> pendingList;
+    private JList<Task> completedList;
+    private JTextField taskField;
+    private JButton addButton, removePendingButton, removeCompletedButton;
 
-public class TodoListApp {
-    
+    public TodoListApp() {
+        setTitle("TodoListApp");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null); // Center window
+    }
 }
