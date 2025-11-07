1afd1a76d89d7d6483fe3d007d631128b0d8bc89
Sasank5716 <sasanklama5716@gmail.com>
2025-11-07 18:18:38 +0545

    Add static exchangeRates map

--- CurrencyConverterApp/CurrencyConverterApp.java
+++ CurrencyConverterApp/CurrencyConverterApp.java
@@ -1,9 +1,22 @@
 package CurrencyConverterApp;
+
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CurrencyConverterApp {
-public CurrencyConverterApp() {
+public class CurrencyConverterApp extends JFrame {
+    private static final Map<String, Double> exchangeRates = new HashMap<>();
+
+    static {
+        exchangeRates.put("USD", 1.0);
+        exchangeRates.put("EUR", 0.92);
+        exchangeRates.put("GBP", 0.78);
+        exchangeRates.put("INR", 82.5);
+        exchangeRates.put("JPY", 146.7);
+    }
+
+    public CurrencyConverterApp() {
         setTitle("Currency Converter");
         setSize(400, 250);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -16,5 +29,5 @@ public CurrencyConverterApp() {
         CurrencyConverterApp app = new CurrencyConverterApp();
         app.setVisible(true);
     }
-    
+
 }
