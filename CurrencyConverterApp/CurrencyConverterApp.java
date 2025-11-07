6bd634038525e80709326432ea28d2a998b1c029
Sasank5716 <sasanklama5716@gmail.com>
2025-11-07 18:16:34 +0545

    Initialize project with main JFrame setup for Currency Converter app

--- CurrencyConverterApp/CurrencyConverterApp.java
+++ CurrencyConverterApp/CurrencyConverterApp.java
@@ -1,6 +1,20 @@
 package CurrencyConverterApp;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
 public class CurrencyConverterApp {
+public CurrencyConverterApp() {
+        setTitle("Currency Converter");
+        setSize(400, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
 
+    public static void main(String[] args) {
+        CurrencyConverterApp app = new CurrencyConverterApp();
+        app.setVisible(true);
+    }
     
 }
