31a57eb990403ab69201ec24575ea0cad061ac82
Sasank5716 <sasanklama5716@gmail.com>
2025-11-17 17:17:29 +0545

    Draw graphics for map generator

--- BrickBreakerGame/BrickBreakerGame.java
+++ BrickBreakerGame/BrickBreakerGame.java
@@ -105,5 +105,23 @@ public class MapGenerator {
         brickHeight = 150 / row;
     }
 
+    public void draw(Graphics2D g) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] > 0) {
+                    g.setColor(new Color(255, 105, 180));
+                    g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(new Color(255, 20, 147));
+                    g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+                }
+            }
+        }
+    }
+
+    public void setBrickValue(int value, int row, int col) {
+        map[row][col] = value;
+    }
     
 }
