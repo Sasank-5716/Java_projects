e491a50d4c664699dee87f1e4fc8117414c4f5ab
Sasank5716 <sasanklama5716@gmail.com>
2025-11-17 17:09:04 +0545

    Add game state variables and timer setup

--- BrickBreakerGame/BrickBreakerGame.java
+++ BrickBreakerGame/BrickBreakerGame.java
@@ -2,15 +2,27 @@ package BrickBreakerGame;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.event.KeyListener;
+import java.util.Timer;
 import java.awt.event.KeyEvent;
 
 public class BrickBreakerGame extends JPanel implements KeyListener {
+    private boolean play = false;
+    private Timer timer;
+    private int delay = 8;
+
     public BrickBreakerGame() {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+        timer = new Timer();
+        timer.start()
     }
 
+     @Override
+    public void actionPerformed(ActionEvent e) {
+        timer.start();
+        repaint();
+    }
     @Override
     public void keyTyped(KeyEvent e) {}
     @Override
