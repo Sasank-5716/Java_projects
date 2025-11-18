6610d6871dd2ae20b10b2275983a8dabda0c9ede
Sasank5716 <sasanklama5716@gmail.com>
2025-11-18 11:47:18 +0545

    Fixed use of Timer and imports

--- BrickBreakerGame/BrickBreakerGame.java
+++ BrickBreakerGame/BrickBreakerGame.java
@@ -1,20 +1,18 @@
 package BrickBreakerGame;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.event.KeyListener;
-import java.util.Timer;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BrickBreakerGame extends JPanel implements KeyListener, ActionListener {
     private boolean play = false;
@@ -37,7 +35,7 @@ public class BrickBreakerGame extends JPanel implements KeyListener, ActionListe
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        timer = new Timer();
+        timer = new Timer(delay, this);
         timer.start()
     }
 
