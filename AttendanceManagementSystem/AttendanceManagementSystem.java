34fd30978576450a3b9fa8bf5cc5e3910666bfa2
Sasank5716 <sasanklama5716@gmail.com>
2025-11-19 18:50:24 +0545

    Create top button panel container

--- AttendanceManagementSystem/AttendanceManagementSystem.java
+++ AttendanceManagementSystem/AttendanceManagementSystem.java
@@ -1,15 +1,26 @@
 package AttendanceManagementSystem;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 
 public class AttendanceManagementSystem extends JFrame {
+    private JPanel topPanel;
+
     public AttendanceManagementSystem() {
         setTitle("Attendance Management System");
         setSize(800, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
+    }
+
+    private void createTopPanel() {
+        topPanel = new JPanel();
+        topPanel.setBackground(new Color(34, 49, 63));
+        topPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        topPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 5));
     }
 
     public static void main(String[] args) {
@@ -19,4 +30,3 @@ public class AttendanceManagementSystem extends JFrame {
         });
     }
 }
-
