83aead12d0cf3b275756b23fd60a7de2837d9717
Sasank5716 <sasanklama5716@gmail.com>
2025-11-23 12:26:55 +0545

    Basic JFrame and Main Class setup

--- OnlineVotingSystem/OnlineVotingSystem.java
+++ OnlineVotingSystem/OnlineVotingSystem.java
@@ -1,5 +1,24 @@
 package OnlineVotingSystem;
 
-public class OnlineVotingSystem {
-    
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import java.awt.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OnlineVotingSystem extends JFrame {
+    public OnlineVotingSystem() {
+        setTitle("Online Voting System");
+        setSize(700, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            OnlineVotingSystem app = new OnlineVotingSystem();
+            app.setVisible(true);
+        });
+    }
+
 }
