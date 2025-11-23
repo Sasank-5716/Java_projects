cdb0612ac08380c7ddf8b36b463b606b608030c9
Sasank5716 <sasanklama5716@gmail.com>
2025-11-23 12:31:03 +0545

    Create Candidate Class with Votes Tracking

--- OnlineVotingSystem/OnlineVotingSystem.java
+++ OnlineVotingSystem/OnlineVotingSystem.java
@@ -70,3 +70,18 @@ class User {
         this.votedCandidate = candidate;
     }
 }
+
+// Candidate class with vote count
+class Candidate {
+    private String name;
+    private int votes;
+
+    public Candidate(String name) {
+        this.name = name;
+        this.votes = 0;
+    }
+
+    public String getName() { return name; }
+    public int getVotes() { return votes; }
+    public void incrementVotes() { votes++; }
+}
