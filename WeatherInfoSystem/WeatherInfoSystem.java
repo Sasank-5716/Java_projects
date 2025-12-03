a313404d0e194b84226181e771a5551461ece606
Sasank5716 <sasanklama5716@gmail.com>
2025-12-03 21:41:54 +0545

    Defined purple color theme constants & added UI component field declarations

--- WeatherInfoSystem/WeatherInfoSystem.java
+++ WeatherInfoSystem/WeatherInfoSystem.java
@@ -11,5 +11,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class WeatherInfoSystem extends JFrame {
-    
+    private JComboBox<String> cityComboBox;
+    private JTextArea weatherDisplay;
+    private JButton fetchButton;
+    private final Color PURPLE_PRIMARY = new Color(138, 43, 226);
+    private final Color PURPLE_LIGHT = new Color(218, 177, 218);
+    private final Color PURPLE_DARK = new Color(75, 0, 130);
+
 }
