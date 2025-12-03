cc56e7036662570bda2fe5f0ba0c89510d930f3f
Sasank5716 <sasanklama5716@gmail.com>
2025-12-03 21:39:18 +0545

    Initial commit for WeatherInfoSystem

---
+++ WeatherInfoSystem/WeatherInfoSystem.java
@@ -0,0 +1,15 @@
+package WeatherInfoSystem;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherInfoSystem extends JFrame {
+    
+}
