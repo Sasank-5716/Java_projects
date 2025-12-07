bd1af8e679041eded8f05ea74c26eb0c3d51a7ca
Sasank5716 <sasanklama5716@gmail.com>
2025-12-07 17:26:14 +0545

    Add getters/ setters for id and name

--- StudentManagementSystem/StudentManagementSystem.java
+++ StudentManagementSystem/StudentManagementSystem.java
@@ -18,6 +18,22 @@ public class StudentManagementSystem {
             this.grade = grade;
             this.attendance = attendance;
         }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
 }
